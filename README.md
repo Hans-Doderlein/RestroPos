6628fc10af130ff83cece36bd50fe72aaf65b573
HND <hans.doderlein99@gmail.com>
2023-09-05 23:51:09 -0400

    intitial commit with basic setup for project

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # project-2
+
+## Link ->
+
+## Screenshot ->
+
+![License](https://img.shields.io/badge/license-MIT-blue)
+
+## Table of Contents
+
+[Description](#description)
+
+[Installation](#installation)
+
+[Usage](#usage)
+
+[License](#license)
+
+[Contributing](#contributing)
+
+[Questions](#questions)
+
+## Description
+
+# Installation
+
+## Usage
+
+## License
+
+MIT License
+
+## Contributing
+
+Collaborators can upload a pull request of their own branch and the request will be read by the repo owner.
+
+## Questions
+
+For any further questions please email me at hans.doderlein99@gmail.com
+
+For my repo, visit https://github.com/Hans-Doderlein
---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controller');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
+//set up express app
+const app = express();
+//set up port
+const PORT = process.env.PORT || 8000;
+
+//create session
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+        maxAge: 10 * 60 * 1000
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+//set up view engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//middelware
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+//sync and start the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
