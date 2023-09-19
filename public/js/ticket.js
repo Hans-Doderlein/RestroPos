793103a24a89bbeefb30bfbbc879a37786b6fa42
HND <hans.doderlein99@gmail.com>
2023-09-19 16:18:18 -0400

    trycatch made

--- public/js/ticket.js
+++ public/js/ticket.js
@@ -1,43 +1,47 @@
 const plusButton = document.querySelectorAll('#addToCart');
-plusButton.forEach((index)=>{
-index.addEventListener("click",addToCart)
-})
+plusButton.forEach((index) => {
+  index.addEventListener('click', addToCart);
+});
 
-const ticketPrice = document.getElementById('ticketTotal')
+const ticketPrice = document.getElementById('ticketTotal');
 
-const itemDisplay = document.getElementById('currentItems')
+const itemDisplay = document.getElementById('currentItems');
 
-let total=0;
-let items=[];
-async function addToCart (event) {
-    event.preventDefault();
-    console.log(this.dataset.id)
-    //this.name returns name of dish
-    //this.price returns price of dish
-    const itemname = document.createElement('p');
-    itemname.innerText =this.name;
-    itemDisplay.append(itemname)
+let total = 0;
+let items = [];
+async function addToCart(event) {
+  event.preventDefault();
+  console.log(this.dataset.id);
+  //this.name returns name of dish
+  //this.price returns price of dish
+  const itemname = document.createElement('p');
+  itemname.innerText = this.name;
+  itemDisplay.append(itemname);
 
-    total+= Number(this.value)
-   
-    items.push(this.dataset.id)
+  total += Number(this.value * 1.07);
 
-    ticketPrice.textContent =(total*1.07).toFixed(2)
+  items.push(this.dataset.id);
 
-    // display price and name in the aside
+  ticketPrice.textContent = total.toFixed(2);
+
+  // display price and name in the aside
 }
 
-const checkout = document.getElementById("checkout-button")
-checkout.addEventListener('click',async (e)=>{
-    e.preventDefault();
+const checkout = document.getElementById('checkout-button');
+checkout.addEventListener('click', async (e) => {
+  e.preventDefault();
 
+  try {
     await fetch('/orders/new', {
-        method:'post',
-        body:JSON.stringify({total,items}),
-        headers:{"content-type":"application/json"}
-    })
+      method: 'post',
+      body: JSON.stringify({ total, items }),
+      headers: { 'content-type': 'application/json' }
+    });
+  } catch (error) {
+    console.log('error:', error);
+  }
 
-    items=[];
-    total=0;
-    document.location.replace('/menu')
-})
+  items = [];
+  total = 0;
+  document.location.replace('/menu');
+});
